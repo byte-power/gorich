0e4cd24d8df910acee6615d6e2413a57f49d685b
zhoufeng1989 <zhoufengloop@gmail.com>
2020-10-27 20:34:12 +0800

    Update task package.

--- task/task.go
+++ task/task.go
@@ -7,39 +7,57 @@ import (
 	"time"
 )
 
+const (
+	defaultConcurrentWorkerCount = 100
+	defaultQueueSize             = 500
+)
+
+var defaultScheduler = NewScheduler(defaultQueueSize, defaultConcurrentWorkerCount)
+
 type Scheduler struct {
-	jobs        map[string]ScheduledJob
-	jobCount    int
-	workerCount int
-	queue       chan ScheduledJob
+	jobs                  map[string]Job
+	concurrentWorkerCount int
+	queue                 chan Job
 }
 
-func (scheduler *Scheduler) AddPeriodicJob(name string, cron *Cron, jobFunction interface{}, params ...interface{}) (ScheduledJob, error) {
-	scheduledJob := ScheduledJob{
-		name:        name,
-		jobFunction: jobFunction,
-		params:      params,
-		cron:        cron,
+func NewScheduler(queueSize, workerCount int) Scheduler {
+	return Scheduler{
+		jobs:                  make(map[string]Job, 0),
+		concurrentWorkerCount: workerCount,
+		queue:                 make(chan Job, queueSize),
 	}
-	scheduler.jobs[name] = scheduledJob
-	return scheduledJob, nil
 }
 
-func (scheduler *Scheduler) AddRunOnceJob(name string, delay time.Duration, jobFunction interface{}, params ...interface{}) (ScheduledJob, error) {
-	scheduledJob := ScheduledJob{
-		name:        name,
-		delay:       delay,
-		jobFunction: jobFunction,
-		params:      params,
+func (scheduler *Scheduler) jobCount() int {
+	return len(scheduler.jobs)
+}
+
+func (scheduler *Scheduler) AddPeriodicJob(name string, function interface{}, params ...interface{}) *PeriodicJob {
+	job := &PeriodicJob{
+		name:     name,
+		function: function,
+		params:   params,
+		cron:     &Cron{timezone: time.Local},
 	}
-	scheduler.jobs[name] = scheduledJob
-	return scheduledJob, nil
+	scheduler.jobs[name] = job
+	return job
 }
 
-func (scheduler *Scheduler) getRunnableJobs() []ScheduledJob {
-	runnableJobs := []ScheduledJob{}
+func (scheduler *Scheduler) AddRunOnceJob(name string, function interface{}, params ...interface{}) *OnceJob {
+	job := &OnceJob{
+		name:     name,
+		function: function,
+		params:   params,
+		delay:    0,
+	}
+	scheduler.jobs[name] = job
+	return job
+}
+
+func (scheduler *Scheduler) getRunnableJobs(t time.Time) []Job {
+	runnableJobs := []Job{}
 	for _, job := range scheduler.jobs {
-		if job.isReady(time.Now()) {
+		if job.IsRunnable(t) {
 			runnableJobs = append(runnableJobs, job)
 		}
 	}
@@ -48,21 +66,22 @@ func (scheduler *Scheduler) getRunnableJobs() []ScheduledJob {
 
 func (scheduler *Scheduler) Start() {
 	fmt.Println("start scheduler")
-	go scheduler.startWorkers(scheduler.workerCount)
+	go scheduler.startWorkers(scheduler.concurrentWorkerCount)
 	ticker := time.NewTicker(1 * time.Second)
 	for {
 		select {
-		case <-ticker.C:
-			scheduler.QueueRunnableJobs()
+		case tickerTime := <-ticker.C:
+			scheduler.QueueRunnableJobs(tickerTime)
 		}
 		fmt.Println("finish for in Start")
 	}
 }
 
-func (scheduler *Scheduler) QueueRunnableJobs() {
-	runnableJobs := scheduler.getRunnableJobs()
+func (scheduler *Scheduler) QueueRunnableJobs(t time.Time) {
+	runnableJobs := scheduler.getRunnableJobs(t)
 	for _, job := range runnableJobs {
 		scheduler.queue <- job
+		job.ScheduledAt(t)
 	}
 }
 
@@ -77,49 +96,65 @@ func (scheduler *Scheduler) startWorkers(count int) {
 	}
 }
 
-type ScheduledJob struct {
+func Once(name string, function interface{}, params ...interface{}) *OnceJob {
+	return defaultScheduler.AddRunOnceJob(name, function, params)
+}
+
+func Periodic(name string, function interface{}, params ...interface{}) *PeriodicJob {
+	return defaultScheduler.AddPeriodicJob(name, function, params)
+}
+
+type Job interface {
+	IsRunnable(time.Time) bool
+	ScheduledAt(time.Time)
+	Run()
+}
+
+type OnceJob struct {
 	name          string
-	jobFunction   interface{}
+	function      interface{}
 	params        []interface{}
-	cron          *Cron
-	scheduledType string
 	delay         time.Duration
-	nextRun       time.Time
 	scheduledTime time.Time
 }
 
-func (job *ScheduledJob) isReady(t time.Time) bool {
-	scheduledTime := job.scheduledTime
-	var unit time.Duration
-	var at time.Duration
-	switch job.cron.intervalType {
-	case intervalDay:
-		unit = 24 * time.Hour
-		at = time.Duration(t.Hour())*time.Hour +
-			time.Duration(t.Minute())*time.Minute +
-			time.Duration(t.Second())*time.Second
-	case intervalHour:
-		unit = time.Hour
-		at = time.Duration(t.Minute())*time.Minute +
-			time.Duration(t.Second())*time.Second
-	case intervalMinute:
-		unit = time.Minute
-		at = time.Duration(t.Second()) * time.Second
-	case intervalSecond:
-		unit = time.Second
-		at = 0 * time.Second
-	}
+func (job *OnceJob) Delay(delay time.Duration) *OnceJob {
+	job.delay = delay
+	return job
+}
+
+func (job *OnceJob) IsRunnable(t time.Time) bool {
+	return true
+}
+
+func (job *OnceJob) ScheduledAt(t time.Time) {
+	job.scheduledTime = t
+}
+
+func (job *OnceJob) Run() {
+	callJobFuncWithParams(job.function, job.params)
+}
+
+type PeriodicJob struct {
+	name          string
+	function      interface{}
+	params        []interface{}
+	cron          *Cron
+	scheduledTime time.Time
+}
+
+func (job *PeriodicJob) IsRunnable(t time.Time) bool {
+	at := getAtTime(job.cron.intervalType, t)
 	var isReady bool
-	if scheduledTime.IsZero() {
-		if job.cron.at == 0 {
+	// scheduledTime.IsZero == true if the job has not been sheduled yet.
+	if job.scheduledTime.IsZero() {
+		if (job.cron.at == 0) || (at == job.cron.at) {
 			isReady = true
+		} else {
+			isReady = false
 		}
-		if at == job.cron.at {
-			isReady = true
-		}
-		isReady = false
 	} else {
-		if scheduledTime.Add(time.Duration(job.cron.interval)*unit).Add(at) == t {
+		if job.scheduledTime.Add(job.cron.Duration()) == t {
 			isReady = true
 		} else {
 			isReady = false
@@ -128,31 +163,63 @@ func (job *ScheduledJob) isReady(t time.Time) bool {
 	return isReady
 }
 
-func (job *ScheduledJob) Run() {
-	callJobFuncWithParams(job.jobFunction, job.params)
+func (job *PeriodicJob) ScheduledAt(t time.Time) {
+	job.scheduledTime = t
 }
 
-func NewScheduler(queueSize int) Scheduler {
-	return Scheduler{
-		jobs:        make(map[string]ScheduledJob, 0),
-		workerCount: 1,
-		queue:       make(chan ScheduledJob, queueSize),
+func (job *PeriodicJob) EverySeconds(second int) *PeriodicJob {
+	job.cron.intervalType = intervalSecond
+	job.cron.interval = second
+	return job
+}
+
+func (job *PeriodicJob) EveryMinutes(minute int) *PeriodicJob {
+	job.cron.intervalType = intervalMinute
+	job.cron.interval = minute
+	return job
+}
+
+func (job *PeriodicJob) EveryHours(hour int) *PeriodicJob {
+	job.cron.intervalType = intervalHour
+	job.cron.interval = hour
+	return job
+}
+
+func (job *PeriodicJob) EveryDays(day int) *PeriodicJob {
+	job.cron.intervalType = intervalDay
+	job.cron.interval = day
+	return job
+}
+
+func getAtTime(intervalType IntervalType, t time.Time) time.Duration {
+	var at time.Duration
+	switch intervalType {
+	case intervalWeek:
+		at = time.Duration(t.Hour())*time.Hour +
+			time.Duration(t.Minute())*time.Minute +
+			time.Duration(t.Second())*time.Second
+	case intervalDay:
+		at = time.Duration(t.Hour())*time.Hour +
+			time.Duration(t.Minute())*time.Minute +
+			time.Duration(t.Second())*time.Second
+	case intervalHour:
+		at = time.Duration(t.Minute())*time.Minute +
+			time.Duration(t.Second())*time.Second
+	case intervalMinute:
+		at = time.Duration(t.Second()) * time.Second
+	case intervalSecond:
+		at = time.Duration(0)
 	}
+	return at
+}
+
+func (job *PeriodicJob) Run() {
+	callJobFuncWithParams(job.function, job.params)
 }
 
 type Queue interface{}
 
 type Worker interface{}
-
-// type Job struct {
-// 	name        string
-// 	jobFunction interface{}
-// 	params      []interface{}
-// }
-
-// func NewJob(name string, jobFunction interface{}, params ...interface{}) (Job, error) {
-// 	return Job{name: name, jobFunction: jobFunction, params: params}, nil
-// }
 
 func callJobFuncWithParams(jobFunc interface{}, params []interface{}) ([]reflect.Value, error) {
 	f := reflect.ValueOf(jobFunc)
@@ -249,6 +316,23 @@ func (cron *Cron) AtSecondInMinute(second int) error {
 	}
 	cron.at = time.Duration(second) * time.Second
 	return nil
+}
+
+func (cron *Cron) Duration() time.Duration {
+	var duration time.Duration
+	switch cron.intervalType {
+	case intervalWeek:
+		duration = time.Duration(cron.interval) * 7 * 24 * time.Hour
+	case intervalDay:
+		duration = time.Duration(cron.interval) * 24 * time.Hour
+	case intervalHour:
+		duration = time.Duration(cron.interval) * time.Hour
+	case intervalMinute:
+		duration = time.Duration(cron.interval) * time.Minute
+	case intervalSecond:
+		duration = time.Duration(cron.interval) * time.Second
+	}
+	return duration
 }
 
 func isValidHour(hour int) bool {
